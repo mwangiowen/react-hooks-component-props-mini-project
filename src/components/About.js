3198ac078c00cb10dbcbecbb13a13546d25b72d7
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-30 10:38:31 +0300

    job done

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import blogData from '../data/blog'
+ function About({image = "https://via.placeholder.com/215", about}) {
+  return (
+   <aside>
+    <img src={blogData.image}alt="blog logo"/>
+    <p>{blogData.about}</p>
+   </aside>
+  )
+}
+export default About
